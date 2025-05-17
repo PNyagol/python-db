33f4761f1af32e549a1b50cdd4b7cb453df74321
PNyagol <nyagolpeter@gmail.com>
2025-05-17 12:03:17 +0300

    normal practice

---
+++ insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+
+INSERT INTO cats (name, age, breed) VALUES ('Hanah', 1, 'Taby');
+
+-- insert into owners
+
+
+INSERT INTO owners (name) VALUES ('Peter Nyagol');
---
+++ select.sql
@@ -0,0 +1,4 @@
+.mode column
+-- SELECT * FROM owners;
+
+SELECT * FROM cats WHERE owner_id = 1;
